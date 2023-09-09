61e5f438378dea36acfd553ca9e7e65d37de8904
Anıl Çelik <anlclk11@hotmail.com>
2023-09-09 19:46:49 +0300

    Icon eklendi.

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,19 +20,19 @@ const routes = {
     }
 };
 
+// sayfalar arası geçiş yapıldığında moda ismi başta kalması için
 const pageTitlePrefix = 'Moda | ';
 
 async function handleRoute() {
-    let url = location.hash.substring(1);
-    document.title = pageTitlePrefix + routes[url].title;
-    if (url.length < 1) {
+    let url = location.hash.substring(1); //location adresini locastion hash ile bulup substring ile ilk kısmı sildik yani '#'
+    document.title = pageTitlePrefix + routes[url].title; // sayfa değiştiğin sayfanın başındaki kısmı dursun, routes kısmında url ne olursa onun başlığını alsın
+    if (url.length < 1) { // burada küçük olduğunda '/' bu olur demektir.
         url = '/';
     }
     
-    const route = routes[url];
+    const route = routes[url]; 
     if (route && route.callback) {
-        clearContent();
-        // document.title = route.title;
+        clearContent(); //sayfa içeriğini temizlemek için
         route.callback();
     } else {
         'Sayfa bulunamadı'
@@ -42,33 +42,33 @@ async function handleRoute() {
 addEventListener('hashchange', handleRoute);
 
 function clearContent() {
-    const productContainer = document.querySelector('.productContainer');
+    const productContainer = document.querySelector('.productContainer'); //sayfa değiştiğinde sayfa içeriği temizledim
     productContainer.innerHTML = '';
 }
 
 async function loadHomePage() {
-    const allProducts = await fetchData();
+    const allProducts = await fetchData(); //anasayfa tüm ürünleri göstermek için
     displayProducts(allProducts);
 }
 
 async function loadManProducts() {
     const allProducts = await fetchData();
-    const manProducts = allProducts.filter(product => product.gender === 'Erkek');
+    const manProducts = allProducts.filter(product => product.gender === 'Erkek'); //erkek sekmesine geçtiğimde tüm ürünlerde cinsiyeti erkek olanları çekmek için kullandım
     displayProducts(manProducts);
 }
 
 async function loadWomanProducts() {
     const allProducts = await fetchData();
-    const womanProducts = allProducts.filter(product => product.gender === 'Kadın');
+    const womanProducts = allProducts.filter(product => product.gender === 'Kadın'); //kadın sekmesine geçtiğimde tüm ürünlerde cinsiyeti kadın olanları çekmek için kullandım
     displayProducts(womanProducts);
 }
 
 async function loadBagProducts() {
     const allProducts = await fetchData();
-    const bagProducts = allProducts.filter(product => product.type === 'Çanta');
+    const bagProducts = allProducts.filter(product => product.type === 'Çanta'); //çanta ürünlerini ürünün tipi ile ilgili kısımdan bulup çanta sekmesine geçtiğimde bu callbak'i çağırdım.
     displayProducts(bagProducts)
 }
-
+//her sekmede data çekmek için kısayol kullandım
 async function fetchData() {
     return await fetch('https://rceavizwdfumfwcligdo.supabase.co/rest/v1/product', {
         headers: {
@@ -78,7 +78,7 @@ async function fetchData() {
         }
     }).then(x => x.json());
 }
-
+//ürünlerin gösterileceği etiketi yakalayıp crateproducktcard ile containera ekledim.
 function displayProducts(products) {
     const productContainer = document.querySelector('.productContainer');
     products.forEach(product => {
@@ -86,7 +86,7 @@ function displayProducts(products) {
         productContainer.appendChild(productCard);
     });
 }
-
+//productcontainerin içine eklenecek olan cardların içeriğini ve datadan gerekli bilgileri html etiketlerine yazdırdım
 function createProductCard(product) {
     const productCard = document.createElement('div');
     productCard.classList.add('card');
@@ -103,6 +103,7 @@ function createProductCard(product) {
     return productCard;
 }
 
+//sayfa açıldığında anasayfayı yüklemesini, çalıştırmasını istedim
 loadHomePage();
 
 
